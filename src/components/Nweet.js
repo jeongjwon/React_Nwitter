3040858cbdee0046363b336716d5f15a461cf897
jeongjwon <76391160+jeongjwon@users.noreply.github.com>
2022-09-30 20:01:08 +0900

    Delete & Update Function

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,15 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { dbService } from "fbase";
+import { doc, deleteDoc, updateDoc } from "firebase/firestore";
 
 function Nweet({ nweetObj, isOwner }) {
     //isOwner 자신의 nweetObj의 uid와 같다면 삭제와 수정 가능
+    const [editing, setEditing] = useState(false);
+    const [newNweet, setNewNweet] = useState(nweetObj.text);
+    const NweetTextRef = doc(dbService, "nweets", `${nweetObj.id}`);
+    
+
+    //Delete Nweet Click!
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure you want to delete this nweet?");
+        console.log(ok);
+        if (ok) {
+            //delete nweet
+            await deleteDoc(NweetTextRef);
+        }
+    }
+
+    const toggleEditing = () => {
+        //editing 이 false 인지 true 인지 edit 버튼을 누를때 마다 바뀜
+        //true 일 경우에만 return에서 보여짐
+        setEditing(prev => !prev);
+    }
+
+    const onSubmit = async(event) => {
+        event.preventDefault();
+        console.log(nweetObj, newNweet);
+        await updateDoc(NweetTextRef, { text: newNweet });
+        //NweetTextRef를 text만 수정해줌
+        setEditing(false);
+        //submit 을 하면 다시 editing을 false 로
+        
+    }
+    const onChange = (event) => {
+        const { target: { value }, } = event;
+        setNewNweet(value);
+        
+
+    }
     return (
         <div key={nweetObj.id}>
-            <h4>{nweetObj.text}</h4>
-            {isOwner && (
-                <> < button > Delete Nweet</button>
-                    < button > Edit Nweet</button> </>
+            {
+                editing ? 
+                    (
+                        <>
+                            <form onSubmit={onSubmit}>
+                                <input
+                                    type="text"
+                                    placeholder="Edit your nweet"
+                                    value={newNweet}
+                                    onChange={onChange}
+                                    required />
+                                <input type="submit" value="Update Nweet" />
+                        </form>
+                        <button onClick={toggleEditing}>Cancel</button>
+                        </>
+                    ) :
+                    (
+                        <>
+                        <h4>{nweetObj.text}</h4>
+                     {isOwner && (
+                <>
+                    <button onClick={onDeleteClick}> Delete Nweet</button>
+                    <button onClick={toggleEditing}> Edit Nweet</button>
+                </>
+                )
+                            }
+                        </>
+                        
                     )
-                }
+            }
         </div>
     );
 
